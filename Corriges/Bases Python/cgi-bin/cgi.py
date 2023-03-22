928833a52a9a3961a0bfe083601776f9764b6239
Twixy Twixy <twixy@free.fr>
2023-03-22 16:55:05 +0100

    correction mise en page

--- Corriges/Bases Python/cgi-bin/cgi.py
+++ Corriges/Bases Python/cgi-bin/cgi.py
@@ -9,12 +9,12 @@ name = "Xavier"
 # Content-type: text/html  (à mettre si besoin au debut du code html)
 print(f"""
 <html>
-<head> <title> Titre de la page
-</title> </head>
-<style>
-</style>
-<body>
-<h3> Mon nom est {name} </h3>
-</body>
+    <head>
+        <title> Titre de la page </title>
+    </head>
+    <style></style>
+    <body>
+        <h3> Mon nom est {name} </h3>
+    </body>
 </html>
 """)
